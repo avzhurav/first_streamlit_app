af8ba44fd235a47bc33a3e31173f9a36ebb945ef
Alexander Zhuravlev <40874107+avzhurav@users.noreply.github.com>
2022-10-07 12:47:22 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,5 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Dummy tasks from Snowflake')
+streamlit.text('dummy line one')
+streamlit.text('dummy line two')
