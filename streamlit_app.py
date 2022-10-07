7734b614a4429f9c7407f65c774fe2b6089fe720
Alexander Zhuravlev <40874107+avzhurav@users.noreply.github.com>
2022-10-07 12:51:54 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Dummy tasks from Snowflake')
-streamlit.text('dummy line one')
-streamlit.text('dummy line two')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
